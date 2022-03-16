05e5f3c4522bec5d8bd186a6a0c4ee4aaee669a7
Emily Dokken <dokkenemily@gmail.com>
2022-03-15 23:39:11 -0700

    add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 module.exports = {
@@ -26,9 +27,7 @@ module.exports = {
               name(file) {
                 return "[path][name].[ext]";
               },
-              publicPath: function (url) {
-                return url.replace("../", "/assets");
-              },
+              publicPath: ""
             },
           },
           {
@@ -46,10 +45,27 @@ module.exports = {
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the DIST folder
     }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, './'),
+      directory: path.join(__dirname, "./"),
     },
     compress: true,
     port: 8080,
