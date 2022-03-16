a5420931e696acd0e3440e6d90a2191dc0865a4a
Emily Dokken <dokkenemily@gmail.com>
2022-03-15 22:55:00 -0700

    add service worker to serve cached pages while offline

--- webpack.config.js
+++ webpack.config.js
@@ -47,5 +47,12 @@ module.exports = {
       analyzerMode: "static", // the report outputs to an HTML file in the DIST folder
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+    },
+    compress: true,
+    port: 8080,
+  },
   mode: "development",
 };
